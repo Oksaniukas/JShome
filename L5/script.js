a3bacff217b043d63fd0d6f7b85720cc5791c525
Oksaniukas <106347531+Oksaniukas@users.noreply.github.com>
2022-10-01 09:54:57 +0300

    first commit

--- L5/script.js
+++ L5/script.js
@@ -40,7 +40,7 @@ let x = '';
 
 for (let x=0; x < arr.length; x++) {
    if (arr[x]) {
-      x += '' + arr[x] + ' '
+      x += '' + arr[x] + ' ' //+= то же как х+ т.е. возьми себя и прибавь
    }
 }
 console.log(x)
@@ -59,8 +59,12 @@ let arr1 = [0, 'ss', 15, 'rs', 55, false, 233, 'labas', 777, true, 1, 2, 'bb', '
 let arr2 = [];
 
 for ( let x=0; x < arr.length; x++) {
-   if (arr1[x]) {
+   if (arr1[x]) {  //если булевое положительное
        arr2.push(arr1[x])
    }
 }
-console.log(x)
+arr.length = 3;
+arr2.splice(3); //урежьте массив до 3 элементов с помощью splice.
+// или так console.log(arr2.length)=19 arr2.splice(3, arr2.length-3 )
+
+console.log(arr2)
